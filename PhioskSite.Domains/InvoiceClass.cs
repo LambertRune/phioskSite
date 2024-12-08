a561760a1642ddab585c444a26fa1e2d423dda72
lambertR <rune.lambert@student.vives.be>
2024-12-08 10:57:33 +0100

    Add project files.

---
+++ PhioskSite.Domains/InvoiceClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhioskSite.Domains
+{
+    public class InvoiceModel
+    {
+        public int InvoiceNumber { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Address SellerAddress { get; set; }
+        public Address CustomerAddress { get; set; }
+
+        public List<OrderItem> Items { get; set; }
+        public string Comments { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public BTW BTW { get; set; }
+    }
+
+    public class Address
+    {
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public object Email { get; set; }
+        public string Phone { get; set; }
+    }
+    public enum BTW
+    {
+        zes = 6,
+        eenentwintig = 21
+    }
+}
