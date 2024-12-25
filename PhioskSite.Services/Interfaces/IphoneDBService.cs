88c08dcf3f81c29cf7dffc03b468bbbd6d71487f
lambertR <rune.lambert@student.vives.be>
2024-12-25 18:33:00 +0100

    create

---
+++ PhioskSite.Services/Interfaces/IphoneDBService.cs
@@ -0,0 +1,14 @@
+﻿using PhioskSite.Domains.EntitiesDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhioskSite.Services.Interfaces
+{
+    public interface IphoneDBService: IDBService<Phone>
+    {
+        
+    }
+}
