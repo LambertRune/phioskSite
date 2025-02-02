bf73da70f8623cd822a9ade61162789144ab0e6a
lambertR <rune.lambert@student.vives.be>
2025-02-02 22:01:10 +0100

    update

---
+++ phioskSite/Controllers/SocialeMediaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhioskSite.Controllers
+{
+    public class SocialeMediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
